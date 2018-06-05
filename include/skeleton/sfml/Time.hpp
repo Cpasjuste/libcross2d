5a7b0b1fe6d280f9a2c0cb53408ec07184cb4d71
Cpasjuste <cpasjuste@gmail.com>
2018-06-05 17:23:17 +0200

    Time: minor change to fix snes9x..

--- include/skeleton/sfml/Time.hpp
+++ include/skeleton/sfml/Time.hpp
@@ -82,7 +82,7 @@ public:
     ////////////////////////////////////////////////////////////
     // Static member data
     ////////////////////////////////////////////////////////////
-    static const Time Zero; ///< Predefined "zero" time value
+    static const Time Zero_; ///< Predefined "zero" time value
 
 private:
 
