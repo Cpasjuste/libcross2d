ef58f88cdd9647df5dc9b8a31cbb1d5667685d73
Cpasjuste <cpasjuste@gmail.com>
2018-09-28 13:18:25 +0200

    c2dui: remove pfba deps

--- source/skeleton/text.cpp
+++ source/skeleton/text.cpp
@@ -10,8 +10,8 @@ using namespace c2d;
 Text::Text(const std::string &string, const Font &font, unsigned int characterSize)
         : sfml::Text(string, font, characterSize) {
 
-    printf("Text(%p): %s (%ix%i)\n", this, string.c_str(),
-            (int) getLocalBounds().width, (int) getLocalBounds().height);
+    //printf("Text(%p): %s (%ix%i)\n", this, string.c_str(),
+    //        (int) getLocalBounds().width, (int) getLocalBounds().height);
 
     type = TText;
 }
@@ -27,5 +27,5 @@ void Text::draw(Transform &transform) {
 
 Text::~Text() {
 
-    printf("~Text(%p): %s\n", this, getString().c_str());
+    //printf("~Text(%p): %s\n", this, getString().c_str());
 }
