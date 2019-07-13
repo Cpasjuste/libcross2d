b9ec0e61cb92ad3d9296e1e6011e019c7f766286
Cpasjuste <cpasjuste@gmail.com>
2019-07-13 10:42:28 +0200

    freeplay: don't modify screen size here

--- source/platforms/sdl2/sdl2_gl_renderer.cpp
+++ source/platforms/sdl2/sdl2_gl_renderer.cpp
@@ -61,11 +61,6 @@ SDL2Renderer::SDL2Renderer(const Vector2f &size) : GLRenderer(size) {
     printf("OpenGL: %s, GLSL: %s\n", glGetString(GL_VERSION), glGetString(GL_SHADING_LANGUAGE_VERSION));
     initGL();
 
-#ifdef __FREEPLAY__
-    setSize(301, 203);
-    setPosition(9, 4);
-#endif
-
     available = true;
 
     printf("SDL2Renderer(GL)(%p): %ix%i\n", this, (int) getSize().x, (int) getSize().y);
