99fecbea8766f6a6bce51bfbff3a7be023388f15
Cpasjuste <cpasjuste@gmail.com>
2019-10-03 14:15:55 +0200

    test: revert

--- test/main.cpp
+++ test/main.cpp
@@ -12,7 +12,6 @@ int main(int argc, char **argv) {
     auto *renderer = new C2DRenderer(Vector2f(C2D_SCREEN_WIDTH, C2D_SCREEN_HEIGHT));
     renderer->setClearColor(Color::Black);
 
-    /*
     // scale font and texture for different resolution (devices),
     // based on 720p resolution
     float scaling = renderer->getSize().y / 720.0f;
@@ -28,8 +27,7 @@ int main(int argc, char **argv) {
     rect->setOutlineThickness(8 * scaling);
 
     // create a texture and add it to the rect
-    // on any other platform than switch, vita, and 3ds, getRomFsPath returns getDataPath
-    auto *tex = new C2DTexture(renderer->getIo()->getRomFsPath() + "gbatemp.png");
+    auto *tex = new C2DTexture(renderer->getIo()->getDataPath() + "gbatemp.png");
     if (tex->available) {
         tex->setPosition(rect->getSize().x / 2, rect->getSize().y / 2);
         tex->setScale(0.5f * scaling, 0.5f * scaling);
@@ -67,59 +65,6 @@ int main(int argc, char **argv) {
     auto *tweenAlpha = new TweenAlpha(255, 200, 3.0f, TweenLoop::PingPong);
     tweenAlpha->play();
     rect->add(tweenAlpha);
-    */
-
-    C2DTexture *tex = new C2DTexture(renderer->getIo()->getRomFsPath() + "gbatemp.png");
-    C2DObject *obj = new Sprite(tex);
-    renderer->add(obj);
-
-    printf("obj: getSize: %f %f\n", obj->getSize().x, obj->getSize().y);
-    obj->setSize(64, 64);
-    printf("obj: setSize: %f %f\n", obj->getSize().x, obj->getSize().y);
-    
-    printf("obj: getPosition: %f %f\n", obj->getPosition().x, obj->getPosition().y);
-    obj->setPosition(64, 64);
-    printf("obj: setPosition: %f %f\n", obj->getPosition().x, obj->getPosition().y);
-
-    printf("obj: getRotation: %f\n", obj->getRotation());
-    obj->setRotation(30);
-    printf("obj: setRotation: %f\n", obj->getRotation());
-
-    printf("obj: getScale: %f %f\n", obj->getScale().x, obj->getScale().y);
-    obj->setScale(2, 2);
-    printf("obj: setScale: %f %f\n", obj->getScale().x, obj->getScale().y);
-
-    printf("obj: move: %f %f\n", obj->getPosition().x, obj->getPosition().y);
-    obj->move(64, 64);
-    printf("obj: move: %f %f\n", obj->getPosition().x, obj->getPosition().y);
-
-    printf("obj: rotate: %f\n", obj->getRotation());
-    obj->rotate(30);
-    printf("obj: rotate: %f\n", obj->getRotation());
-
-    printf("obj: scale: %f %f\n", obj->getScale().x, obj->getScale().y);
-    obj->scale(0.5, 0.5);
-    printf("obj: scale: %f %f\n", obj->getScale().x, obj->getScale().y);
-
-    printf("obj: getOrigin: %i\n", (int) obj->getOrigin());
-    obj->setOrigin(Origin::Center);
-    printf("obj: getOrigin: %i\n", (int) obj->getOrigin());
-
-    printf("obj: getFillColor: %i\n", (int) obj->getFillColor().toRGBA());
-    obj->setFillColor(Color::Red);
-    printf("obj: setFillColor: %i\n", (int) obj->getFillColor().toRGBA());
-
-    printf("obj: getOutlineColor: %i\n", (int) obj->getOutlineColor().toRGBA());
-    obj->setOutlineColor(Color::Green);
-    printf("obj: setOutlineColor: %i\n", (int) obj->getOutlineColor().toRGBA());
-
-    printf("obj: getOutlineThickness: %f\n", obj->getOutlineThickness());
-    obj->setOutlineThickness(10);
-    printf("obj: setOutlineThickness: %f\n", obj->getOutlineThickness());
-
-    printf("obj: getLocalBounds: %f %f %f %f\n",
-           obj->getLocalBounds().left, obj->getLocalBounds().top,
-           obj->getLocalBounds().width, obj->getLocalBounds().height);
 
     while (true) {
 
