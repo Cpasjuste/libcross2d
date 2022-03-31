73e28892476eec99f11e08549369c76b02356103
cpasjuste <cpasjuste@gmail.com>
2022-03-31 17:24:09 +0200

    object: fix default transformation (for getGlobalPosition, getGlobalBounds), cleanup

--- source/skeleton/object.cpp
+++ source/skeleton/object.cpp
@@ -7,44 +7,37 @@
 using namespace c2d;
 
 void C2DObject::add(C2DObject *object) {
-
-    if (object != nullptr) {
+    if (object) {
         //printf("C2DObject(%p): add(%p)\n", this, C2DObject);
         object->parent = this;
         object->setLayer(object->getLayer());
+        object->transformation = transformation * ((Transformable *) this)->getTransform();
         childs.push_back(object);
     }
 }
 
 void C2DObject::remove(C2DObject *object) {
-
     if (!childs.empty()) {
-        childs.erase(std::remove(
-                childs.begin(), childs.end(), object),
-                     childs.end());
+        childs.erase(std::remove(childs.begin(), childs.end(), object), childs.end());
     }
 }
 
 void C2DObject::add(Tween *tween) {
-
-    if (tween != nullptr) {
+    if (tween) {
         tween->setTransform((Transformable *) this);
         tweens.push_back(tween);
     }
 }
 
 void C2DObject::remove(Tween *tween) {
-
-    if (tween != nullptr && !tweens.empty()) {
-        tweens.erase(std::remove(
-                tweens.begin(), tweens.end(), tween), tweens.end());
+    if (tween && !tweens.empty()) {
+        tweens.erase(std::remove(tweens.begin(), tweens.end(), tween), tweens.end());
     }
 }
 
 bool C2DObject::onInput(Input::Player *players) {
-
-    for (auto &child : childs) {
-        if (child != nullptr && child->isVisible()) {
+    for (auto &child: childs) {
+        if (child && child->isVisible()) {
             if (child->onInput(players)) {
                 return true;
             }
@@ -55,25 +48,22 @@ bool C2DObject::onInput(Input::Player *players) {
 }
 
 void C2DObject::onUpdate() {
-
-    for (auto &child : childs) {
-        if (child != nullptr) {
+    for (auto &child: childs) {
+        if (child) {
             child->onUpdate();
         }
     }
 }
 
 void C2DObject::onDraw(Transform &transform, bool draw) {
-
     //printf("C2DObject(%p): draw\n", this);
-
     if (visibility_current == Visibility::Hidden) {
         return;
     }
 
     // handle tweeners
-    for (auto &tween : tweens) {
-        if (tween != nullptr) {
+    for (auto &tween: tweens) {
+        if (tween) {
             tween->step();
             // hide object if needed
             if (tween->getState() == TweenState::Stopped
@@ -86,8 +76,8 @@ void C2DObject::onDraw(Transform &transform, bool draw) {
     Transform combinedTransform = transformation = transform;
     combinedTransform *= ((Transformable *) this)->getTransform();
 
-    for (auto &child : childs) {
-        if (child != nullptr) {
+    for (auto &child: childs) {
+        if (child) {
             if (child->visibility_current == Visibility::Visible) {
                 child->onDraw(combinedTransform, draw);
             }
@@ -100,12 +90,10 @@ bool C2DObject::isVisible() {
 }
 
 Visibility C2DObject::getVisibility() {
-
     return visibility_wanted;
 }
 
 void C2DObject::setVisibility(Visibility v, bool tweenPlay) {
-
     if (tweenPlay && !tweens.empty()) {
         if (v == Visibility::Visible) {
             // we want the object to be visible (drawn) immediately
@@ -113,8 +101,8 @@ void C2DObject::setVisibility(Visibility v, bool tweenPlay) {
         } else {
             visibility_wanted = Visibility::Hidden;
         }
-        for (auto &tween : tweens) {
-            if (tween != nullptr) {
+        for (auto &tween: tweens) {
+            if (tween) {
                 tween->play(visibility_wanted == Visibility::Visible ?
                             TweenDirection::Forward : TweenDirection::Backward);
                 // reset transform/color to initial values
@@ -123,8 +111,8 @@ void C2DObject::setVisibility(Visibility v, bool tweenPlay) {
         }
     } else {
         visibility_current = visibility_wanted = v;
-        for (auto &tween : tweens) {
-            if (tween != nullptr) {
+        for (auto &tween: tweens) {
+            if (tween) {
                 tween->reset();
             }
         }
@@ -132,10 +120,9 @@ void C2DObject::setVisibility(Visibility v, bool tweenPlay) {
 }
 
 void C2DObject::setAlpha(uint8_t alpha, bool recursive) {
-
     if (recursive) {
-        for (auto &child : childs) {
-            if (child != nullptr) {
+        for (auto &child: childs) {
+            if (child) {
                 child->setAlpha(alpha, recursive);
             }
         }
@@ -159,9 +146,8 @@ static bool sortByLayer(C2DObject *o1, C2DObject *o2) {
 }
 
 void C2DObject::setLayer(int l) {
-
     this->layer = l;
-    if (parent != nullptr) {
+    if (parent) {
         std::stable_sort(parent->childs.begin(),
                          parent->childs.end(),
                          sortByLayer);
@@ -177,12 +163,10 @@ Type C2DObject::getType() const {
 }
 
 C2DObject::~C2DObject() {
-
     //printf("~C2DObject(%p): childs = %i\n", this, (int) childs.size());
-
     // delete tweeners
     for (auto tween = tweens.begin(); tween != tweens.end();) {
-        if (*tween != nullptr) {
+        if (*tween) {
             delete (*tween);
             C2DObject::remove(*tween);
         }
@@ -191,7 +175,7 @@ C2DObject::~C2DObject() {
 
     // delete childs
     for (auto widget = childs.begin(); widget != childs.end();) {
-        if (*widget != nullptr) {
+        if (*widget) {
             if ((*widget)->deleteMode == DeleteMode::Auto) {
                 //printf("\t~C2DObject(%p): delete child(%p)\n", this, *widget);
                 delete (*widget);
@@ -201,7 +185,7 @@ C2DObject::~C2DObject() {
     childs.clear();
 
     // remove from parent
-    if (parent != nullptr) {
+    if (parent) {
         //printf("~C2DObject(%p): remove from parent(%p)\n", this, parent);
         parent->remove(this);
     }
