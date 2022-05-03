b84358247fe79ac34b8fdf9ba1c76bc16a8e951d
cpasjuste <cpasjuste@gmail.com>
2022-05-03 13:32:35 +0200

    sdl2: force rgba8

--- source/platforms/sdl2/sdl2_renderer.cpp
+++ source/platforms/sdl2/sdl2_renderer.cpp
@@ -50,6 +50,10 @@ SDL2Renderer::SDL2Renderer(const Vector2f &s) : GLRenderer(s) {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 #endif
 #endif
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 0);
 
