Cpasjuste/libcross2d

commit ae13a7b3db2d1ed790be5e75da8bcf025870913c

gl: cleanup lock/unlock

include/cross2d/skeleton/texture.h
@@ -33,7 +33,7 @@ namespace c2d {
 
         virtual int lock(uint8_t **pixels, int *pitch = nullptr, IntRect rect = IntRect());
 
-        virtual void unlock(uint8_t *pixels = nullptr) {
+        virtual void unlock(const uint8_t *pixels = nullptr) {
         };
 
         virtual int resize(const Vector2i &size, bool keepPixels = false) { return -1; };
@@ -77,7 +77,6 @@ namespace c2d {
 
         Vector2i m_tex_size;
         Vector2i m_tex_size_pot;
-        IntRect m_unlock_rect = IntRect();
         int m_unpack_row_length = 0;
 #ifdef __3DS__
         bool m_pot = true;

source/platforms/gl2/gl_texture.cpp
@@ -118,16 +118,16 @@ int GLTexture::resize(const Vector2i &size, bool keepPixels) {
     // update texture information
     m_pitch = dst_pitch;
     m_tex_size = {size.x, size.y};
-    m_unlock_rect = {0, 0, size.x, size.y};
+    m_tex_rect = {0, 0, size.x, size.y};
     setSize({(float) size.x, (float) size.y});
-    setTextureRect(m_unlock_rect);
+    setTextureRect(m_tex_rect);
     setFilter(m_filter);
 
     return 0;
 }
 
-void GLTexture::unlock(uint8_t *pixels) {
-    uint8_t *data = (pixels ? pixels : m_pixels) + m_unlock_rect.top * m_pitch + m_unlock_rect.left * m_bpp;
+void GLTexture::unlock(const uint8_t *pixels) {
+    const uint8_t *data = (pixels ? pixels : m_pixels) + m_tex_rect.top * m_pitch + m_tex_rect.left * m_bpp;
 
     //printf("GLTexture::unlock(%p): rect: {%i, %i, %i, %i}, pixels: %p\n",
     //     this, m_unlock_rect.left, m_unlock_rect.top, m_unlock_rect.width, m_unlock_rect.height, data);
@@ -146,7 +146,7 @@ void GLTexture::unlock(uint8_t *pixels) {
 #endif
 
     glTexSubImage2D(GL_TEXTURE_2D, 0,
-                    m_unlock_rect.left, m_unlock_rect.top, m_unlock_rect.width, m_unlock_rect.height,
+                    m_tex_rect.left, m_tex_rect.top, m_tex_rect.width, m_tex_rect.height,
                     m_pixelFormat.format, m_pixelFormat.type, data);
 
     glBindTexture(GL_TEXTURE_2D, 0);

source/skeleton/texture.cpp
@@ -47,7 +47,7 @@ Texture::Texture(const std::string &p) : RectangleShape({0, 0}) {
     // set texture parameters
     m_pitch = m_tex_size_pot.x * m_bpp;
     m_tex_size = {w, h};
-    m_unlock_rect = {0, 0, w, h};
+    m_tex_rect = {0, 0, w, h};
     Texture::setSize((float) w, (float) h);
     Texture::setTexture(this);
     Texture::setFillColor(Color::White);
@@ -80,7 +80,7 @@ Texture::Texture(const unsigned char *buffer, int bufferSize) : RectangleShape({
     // set texture parameters
     m_pitch = m_tex_size_pot.x * m_bpp;
     m_tex_size = {w, h};
-    m_unlock_rect = {0, 0, w, h};
+    m_tex_rect = {0, 0, w, h};
     Texture::setSize((float) w, (float) h);
     Texture::setTexture(this);
     Texture::setFillColor(Color::White);
@@ -112,7 +112,7 @@ Texture::Texture(const Vector2i &size, Format format) : RectangleShape(Vector2f{
     // set texture parameters
     m_pitch = m_tex_size_pot.x * m_bpp;
     m_tex_size = {size.x, size.y};
-    m_unlock_rect = {0, 0, size.x, size.y};
+    m_tex_rect = {0, 0, size.x, size.y};
     Texture::setSize((float) size.x, (float) size.y);
     Texture::setTexture(this);
     Texture::setFillColor(Color::White);
@@ -124,17 +124,15 @@ Texture::Texture(const Vector2i &size, Format format) : RectangleShape(Vector2f{
 }
 
 int Texture::lock(uint8_t **pixels, int *pitch, IntRect rect) {
-    if (rect == IntRect()) {
-        m_unlock_rect = m_tex_rect;
-    } else {
-        m_unlock_rect = rect;
+    if (rect != IntRect()) {
+        m_tex_rect = rect;
     }
 
     if (pitch) {
         *pitch = m_pitch;
     }
 
-    *pixels = m_pixels + m_unlock_rect.top * m_pitch + m_unlock_rect.left * m_bpp;
+    *pixels = m_pixels + m_tex_rect.top * m_pitch + m_tex_rect.left * m_bpp;
 
     //printf("Texture::lock(%p): rect: {%i, %i, %i, %i}, pixels: %p\n",
     //     this, m_unlock_rect.left, m_unlock_rect.top, m_unlock_rect.width, m_unlock_rect.height, *pixels);

include/cross2d/platforms/gl2/gl_texture.h
@@ -20,7 +20,7 @@ namespace c2d {
 
         int resize(const Vector2i &size, bool keepPixels = false) override;
 
-        void unlock(uint8_t *pixels = nullptr) override;
+        void unlock(const uint8_t *pixels = nullptr) override;
 
         void setFilter(Filter filter) override;
 

commit e85184d534149cc4b45e860b5e58eee02f1ed801

texture: revert commit ae13a7b which broke psnes

include/cross2d/skeleton/texture.h
@@ -77,6 +77,7 @@ namespace c2d {
 
         Vector2i m_tex_size;
         Vector2i m_tex_size_pot;
+        IntRect m_unlock_rect = IntRect();
         int m_unpack_row_length = 0;
 #ifdef __3DS__
         bool m_pot = true;

source/platforms/gl2/gl_texture.cpp
@@ -118,16 +118,16 @@ int GLTexture::resize(const Vector2i &size, bool keepPixels) {
     // update texture information
     m_pitch = dst_pitch;
     m_tex_size = {size.x, size.y};
-    m_tex_rect = {0, 0, size.x, size.y};
+    m_unlock_rect = {0, 0, size.x, size.y};
     setSize({(float) size.x, (float) size.y});
-    setTextureRect(m_tex_rect);
+    setTextureRect(m_unlock_rect);
     setFilter(m_filter);
 
     return 0;
 }
 
 void GLTexture::unlock(const uint8_t *pixels) {
-    const uint8_t *data = (pixels ? pixels : m_pixels) + m_tex_rect.top * m_pitch + m_tex_rect.left * m_bpp;
+    const uint8_t *data = (pixels ? pixels : m_pixels) + m_unlock_rect.top * m_pitch + m_unlock_rect.left * m_bpp;
 
     //printf("GLTexture::unlock(%p): rect: {%i, %i, %i, %i}, pixels: %p\n",
     //     this, m_unlock_rect.left, m_unlock_rect.top, m_unlock_rect.width, m_unlock_rect.height, data);
@@ -138,22 +138,22 @@ void GLTexture::unlock(const uint8_t *pixels) {
     glPixelStorei(GL_UNPACK_ROW_LENGTH, m_unpack_row_length);
 #else
     // "fix" missing GL_UNPACK_ROW_LENGTH support on ps4, used for fonts
-    const IntRect rect = m_tex_rect;
+    const IntRect rect = m_unlock_rect;
     if (m_unpack_row_length > 0) {
         // fix missing GL_UNPACK_ROW_LENGTH support on ps4, used for fonts
         data = pixels ? pixels : m_pixels;
-        m_tex_rect = {0, 0, m_tex_size.x, m_tex_size.y};
+        m_unlock_rect = {0, 0, m_tex_size.x, m_tex_size.y};
     }
 #endif
 
     glTexSubImage2D(GL_TEXTURE_2D, 0,
-                    m_tex_rect.left, m_tex_rect.top, m_tex_rect.width, m_tex_rect.height,
+                    m_unlock_rect.left, m_unlock_rect.top, m_unlock_rect.width, m_unlock_rect.height,
                     m_pixelFormat.format, m_pixelFormat.type, data);
 
     glBindTexture(GL_TEXTURE_2D, 0);
 
 #if !defined(GL_UNPACK_ROW_LENGTH)
-    m_tex_rect = rect;
+    m_unlock_rect = rect;
 #endif
 }
 

source/skeleton/texture.cpp
@@ -47,7 +47,7 @@ Texture::Texture(const std::string &p) : RectangleShape({0, 0}) {
     // set texture parameters
     m_pitch = m_tex_size_pot.x * m_bpp;
     m_tex_size = {w, h};
-    m_tex_rect = {0, 0, w, h};
+    m_unlock_rect = {0, 0, w, h};
     Texture::setSize((float) w, (float) h);
     Texture::setTexture(this);
     Texture::setFillColor(Color::White);
@@ -55,7 +55,7 @@ Texture::Texture(const std::string &p) : RectangleShape({0, 0}) {
 
     printf("Texture(%p): tex: %ix%i (pot: %ix%i, rect: %ix%i), bpp: %i, pitch: %i, path: %s\n",
            this, m_tex_size.x, m_tex_size.y, m_tex_size_pot.x, m_tex_size_pot.y,
-           m_tex_rect.width, m_tex_rect.height, m_bpp, m_pitch, m_path.c_str());
+           m_unlock_rect.width, m_unlock_rect.height, m_bpp, m_pitch, m_path.c_str());
 }
 
 Texture::Texture(const unsigned char *buffer, int bufferSize) : RectangleShape({0, 0}) {
@@ -80,7 +80,7 @@ Texture::Texture(const unsigned char *buffer, int bufferSize) : RectangleShape({
     // set texture parameters
     m_pitch = m_tex_size_pot.x * m_bpp;
     m_tex_size = {w, h};
-    m_tex_rect = {0, 0, w, h};
+    m_unlock_rect = {0, 0, w, h};
     Texture::setSize((float) w, (float) h);
     Texture::setTexture(this);
     Texture::setFillColor(Color::White);
@@ -88,7 +88,7 @@ Texture::Texture(const unsigned char *buffer, int bufferSize) : RectangleShape({
 
     printf("Texture(%p): tex: %ix%i (pot: %ix%i, rect: %ix%i), bpp: %i, pitch: %i\n",
            this, m_tex_size.x, m_tex_size.y, m_tex_size_pot.x, m_tex_size_pot.y,
-           m_tex_rect.width, m_tex_rect.height, m_bpp, m_pitch);
+           m_unlock_rect.width, m_unlock_rect.height, m_bpp, m_pitch);
 }
 
 Texture::Texture(const Vector2i &size, Format format) : RectangleShape(Vector2f{0, 0}) {
@@ -112,7 +112,7 @@ Texture::Texture(const Vector2i &size, Format format) : RectangleShape(Vector2f{
     // set texture parameters
     m_pitch = m_tex_size_pot.x * m_bpp;
     m_tex_size = {size.x, size.y};
-    m_tex_rect = {0, 0, size.x, size.y};
+    m_unlock_rect = {0, 0, size.x, size.y};
     Texture::setSize((float) size.x, (float) size.y);
     Texture::setTexture(this);
     Texture::setFillColor(Color::White);
@@ -120,19 +120,19 @@ Texture::Texture(const Vector2i &size, Format format) : RectangleShape(Vector2f{
 
     printf("Texture(%p): tex: %ix%i (pot: %ix%i, rect: %ix%i), bpp: %i, pitch: %i\n",
            this, m_tex_size.x, m_tex_size.y, m_tex_size_pot.x, m_tex_size_pot.y,
-           m_tex_rect.width, m_tex_rect.height, m_bpp, m_pitch);
+           m_unlock_rect.width, m_unlock_rect.height, m_bpp, m_pitch);
 }
 
 int Texture::lock(uint8_t **pixels, int *pitch, IntRect rect) {
     if (rect != IntRect()) {
-        m_tex_rect = rect;
+        m_unlock_rect = rect;
     }
 
     if (pitch) {
         *pitch = m_pitch;
     }
 
-    *pixels = m_pixels + m_tex_rect.top * m_pitch + m_tex_rect.left * m_bpp;
+    *pixels = m_pixels + m_unlock_rect.top * m_pitch + m_unlock_rect.left * m_bpp;
 
     //printf("Texture::lock(%p): rect: {%i, %i, %i, %i}, pixels: %p\n",
     //     this, m_unlock_rect.left, m_unlock_rect.top, m_unlock_rect.width, m_unlock_rect.height, *pixels);
@@ -142,8 +142,8 @@ int Texture::lock(uint8_t **pixels, int *pitch, IntRect rect) {
 
 int Texture::save(const std::string &path) {
     int res;
-    int width = m_tex_rect.width;
-    int height = m_tex_rect.height;
+    int width = m_unlock_rect.width;
+    int height = m_unlock_rect.height;
 
     printf("Texture::save(%p): %ix%i, bpp: %i, pitch: %i\n",
            this, width, height, m_bpp, m_pitch);